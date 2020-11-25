3556d2d0febb667a1cf2e80b93f400581d62bc19
matrizlab <info@matrizlab.com>
2020-11-25 11:58:18 +0100

    start commit

---
+++ src/main/java/Automation/JavaTest1.java
@@ -0,0 +1,15 @@
+package Automation;
+
+import java.io.IOException;
+
+public class JavaTest1 {
+    public static void main(String[] args) throws IOException {
+
+        int i=10;
+        System.out.println("Hello"+"\n");
+        System.out.println(i);
+        System.out.println(i * 10);
+
+
+    }
+}
